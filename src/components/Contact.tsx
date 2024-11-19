827417b2b3600b20e01ec7fce904e5f53925ea7b
anonymous <jbrynnbacuta@gmail.com>
2024-11-19 23:43:10 +0800

    fix bug on feedback

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -110,9 +110,9 @@ export const Contact = () => {
 };
 
 const AnonymousFeedback = () => {
-  const serviceId = process.env.EMAILJS_SERVICE_ID || "";
-  const templateId = process.env.EMAILJS_TEMPLATE_ID || "";
-  const userId = process.env.EMAILJS_USER_ID || "";
+  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID || "";
+  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID || "";
+  const userId = process.env.REACT_APP_EMAILJS_USER_ID || "";
 
   const [feedback, setFeedback] = useState("");
 
